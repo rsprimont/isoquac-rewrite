f14de429b6074b9af587f8d23a84cebc2c9ac7a3
roelsprimontuhasselt <roel.sprimont@uhasselt.be>
2026-01-27 10:54:37 +0100

    Add about page

---
+++ frontend/src/routes/about/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  // No specific script needed for this page
+</script>
+<div class="lg:w-5/6 xl:w-4/6 mx-auto p-8 rounded shadow-md">
+    <h2 class="text-2xl font-semibold text-gray-900 pb-4">About us</h2>
+    <p class="mt-2">
+        IsoQuaC is a statistical analysis tool for proteomics data labeled with isobaric tags. IsoQuaC provides a quick and standardized analysis based on the output data from third-party search engines, like e.g., Proteome Discoverer, MaxQuant, MSFragger, MS-GF+, . The tool is hosted by the Center for Statistics and the Data Science Institute at Hasselt University and receives funding from the Elixir program. 
+    </p>
+    <p class="mt-2">
+        Contact us at <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="mailto:isoquac@uhasselt.be">isoquac@uhasselt.be</a>.
+    </p>
+    <!-- Partner logos -->
+     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+        <div>
+            <img src="/elixir.svg" alt="Elixir logo"/>
+        </div>
+        <div>
+            <img src="/dsi.png" alt="Data Science Institute logo" class="mx-auto h-auto max-w-full" />
+        </div>
+        <div>
+            <img src="/uhasselt.png" alt="UHasselt logo" class="mx-auto h-auto max-w-full" />
+        </div>
+     </div>
+</div>
