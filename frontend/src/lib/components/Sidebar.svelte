ea033ca620935d585d7993ce508331a14a3a0e20
roelsprimontuhasselt <roel.sprimont@uhasselt.be>
2026-01-27 10:50:07 +0100

    Add refrences page

--- frontend/src/lib/components/Sidebar.svelte
+++ frontend/src/lib/components/Sidebar.svelte
@@ -10,6 +10,7 @@
     { name: 'Job', href: '/analysis' },
     { name: 'Design of Experiments', href: '/doe' },
     { name: 'Visualisation', href: '/settings' },
+    { name: 'Tutorials', href: '/tutorials' },
     { name: 'Documentation', href: '/documentation' },
     { name: 'References', href: '/references' },
     { name: 'About', href: '/about' }
@@ -21,16 +22,19 @@
 </script>
 
 <aside 
-  class="h-screen bg-yellow-300 text-white flex flex-col transition-all duration-300 shadow-xl/30
+  class="h-screen bg-yellow-300 text-white flex flex-col transition-all duration-200 shadow-xl/30
   {isOpen ? 'w-64' : 'w-20'}"
 >
   
   <div class="p-4 flex flex-col gap-4">
-    <div class="font-bold text-xl text-slate-900 flex items-center gap-2 h-10 overflow-hidden whitespace-nowrap">
-      <img src="/isoquac.png" alt="IsoQuac Logo" class="h-10 w-10 flex-shrink-0" />
-      <span class="{isOpen ? 'opacity-100' : 'opacity-0'} ml-2 text-xl no-underline hover:drop-shadow-xl/10 transition-opacity duration-300">
+    <div class="font-bold text-xl text-slate-900 flex items-center gap-2 h-11 overflow-hidden whitespace-nowrap">
+    <a href="/" class="inline-block">
+      <img src="/isoquac.png" alt="IsoQuac Logo" class="h-11 w-11 flex-shrink-0 inline-block" />
+
+      <span class="{isOpen ? 'opacity-100' : 'opacity-0'} ml-2 text-xl no-underline hover:drop-shadow-xl/10 transition-opacity duration-200">
         IsoQuac
       </span>
+        </a>
     </div>
 
     <button 
@@ -44,14 +48,14 @@
     </button>
   </div>
 
-  <nav class="flex-1 px-2 py-4 space-y-2">
+  <nav class="flex-1 px-2 pt-2 space-y-2">
     {#each menuItems as item}
       <a 
         href={item.href}
         class="{isOpen ? 'opacity-100 block' : 'opacity-0 hidden'} flex items-center gap-4 px-3 py-3 rounded-lg transition-colors whitespace-nowrap
         {$page.url.pathname === item.href ? 'bg-yellow-100 text-slate-900 font-bold' : 'hover:bg-yellow-200 text-slate-900'}"
       >
-        <span class="{isOpen ? 'opacity-100 block' : 'opacity-0 hidden'} transition-opacity duration-300">
+        <span class="{isOpen ? 'opacity-100 block' : 'opacity-0 hidden'} transition-opacity duration-200">
           {item.name}
         </span>
       </a>
---
+++ frontend/src/routes/references/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+</script>
+
+<div class="lg:w-5/6 xl:w-4/6 mx-auto p-8 rounded shadow-md">
+    <h2 class="text-2xl font-bold text-gray-900 pb-4">References</h2>
+    <p class="mb-4">
+        <b>Disclaimer</b>: IsoQuac does not rely on CONSTANd normalisation as described in reference 2. Instead, an IPFP algorithm is used for matrix normalisation. The row and column constrains are set to the values described in reference 4 to allow for unbalanced isobaric cassettes, i.e., different sample multiplexes can be compared.
+    </p>
+    <p class="mb-4">
+        If you use IsoQuaC in your research, please cite the following publications:
+    </p>
+    <ul class="list-decimal">
+        <li class="mb-4">
+            <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://pubs.acs.org/doi/10.1021/acs.jproteome.9b00072">
+                QCQuan: A Web Tool for the Automated Assessment of Protein Expression and Data Quality of Labeled Mass Spectrometry Experiments
+            </a>
+        </li>
+        <li class="mb-4">
+            <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.sciencedirect.com/science/article/pii/S1535947620335039?via%3Dihub">
+                CONSTANd : A Normalization Method for Isobaric Labeled Spectra by Constrained Optimization
+            </a>
+        </li>
+        <li class="mb-4">
+            <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.sciencedirect.com/science/article/pii/S2212968515000069?via%3Dihub">
+                Detecting significant changes in protein abundance
+            </a>
+        </li>
+        <li class="mb-4">
+            <a class="underline text-blue-600 hover:text-blue-800 visited:text-purple-600" href="https://www.sciencedirect.com/science/article/pii/S0022283621001674">
+                Constrained Standardization of Count Data from Massive Parallel Sequencing
+            </a>
+        </li>
+    </ul>
+</div>
